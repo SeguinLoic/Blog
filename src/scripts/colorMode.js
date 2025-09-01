5f6b3c0f798cb6f0656dad4ff3fc55c5d203df15
Loïc Seguin <seguin.loic@proton.me>
2025-09-01 11:03:23 +0200

    Update colorMode.js

--- src/scripts/colorMode.js
+++ src/scripts/colorMode.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
         // use an aria-label if you are omitting text on the button
         // and using a sun/moon icon, for example
         buttonEl.setAttribute("aria-label", newCta);
-        buttonEl.querySelector('.sr-only').innerText = newCta;
+        buttonEl.innerText = newCta;
     }
 
     /* fonction pour mettre à jour l'attribut sur l'élément html */
@@ -29,31 +29,27 @@ document.addEventListener("DOMContentLoaded", () => {
     /* fonction principale, au chargement de la page */
     function switchColor() {
         /* récupération des éléments */
-        const buttons = document.getElementById("contraste");
+        const button = document.getElementById("contraste");
         const localStorageTheme = localStorage.getItem("theme");
         const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
 
         /* récupération de l'état actuel du thème du site */
         let currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme, systemSettingDark });
 
-        if (buttons) {
-            buttons.forEach((button) => {
-                /* mise à jour du bouton en fonction du thème */
-                updateButton({ buttonEl: button, isDark: currentThemeSetting === "dark" });
-                updateThemeOnHtmlEl({ theme: currentThemeSetting });
+        /* mise à jour du bouton en fonction du thème */
+        updateButton({ buttonEl: button, isDark: currentThemeSetting === "dark" });
+        updateThemeOnHtmlEl({ theme: currentThemeSetting });
 
-                /* event listener au click sur le bouton pour changer de thème */
-                button.addEventListener("click", (event) => {
-                    const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
+        /* event listener au click sur le bouton pour changer de thème */
+        button.addEventListener("click", (event) => {
+        const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
 
-                    localStorage.setItem("theme", newTheme);
-                    updateButton({ buttonEl: button, isDark: newTheme === "dark" });
-                    updateThemeOnHtmlEl({ theme: newTheme });
+        localStorage.setItem("theme", newTheme);
+        updateButton({ buttonEl: button, isDark: newTheme === "dark" });
+        updateThemeOnHtmlEl({ theme: newTheme });
 
-                    currentThemeSetting = newTheme;
-                });
-            });
-        }
+        currentThemeSetting = newTheme;
+      });
     }
     switchColor();
 });
