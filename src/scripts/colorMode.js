73f1590229e1eb3af6d6e2696f1686b30e3e857b
Loïc Seguin <seguin.loic@proton.me>
2025-09-01 10:49:24 +0200

    Update colorMode.js

--- src/scripts/colorMode.js
+++ src/scripts/colorMode.js
@@ -1,44 +1,59 @@
-const button = document.querySelector('#contraste');
-const localStorageTheme = localStorage.getItem("theme");
-const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
-
-function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark }) {
-  if (localStorageTheme !== null) {
-    return localStorageTheme;
-  }
-  if (systemSettingDark.matches) {
-    return "dark";
-  }
-  return "light";
-}
-
-let currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme, systemSettingDark });
-
-window.addEventListener("load", () => {
-    console.log("load");
-    if (localStorageTheme !== currentThemeSetting) {
-        console.log("different");
-        return document.querySelector("html").setAttribute("data-theme", currentThemeSetting);
+document.addEventListener("DOMContentLoaded", () => {
+    /* fonction pour récupérer l'état des paramètres du thème */
+    function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark }) {
+        if (localStorageTheme !== null) {
+            return localStorageTheme;
+        }
+        if (systemSettingDark.matches) {
+            return "dark";
+        } else {
+            /* defaut */
+            return "light";
+        }
     }
-})
 
-button.addEventListener("click", () => {
-  const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
+    /* fonction pour mettre à jour l'aria-label et le texte du bonton */
+    function updateButton({ buttonEl, isDark }) {
+        const newCta = isDark ? "Thème clair" : "Thème sombre";
+        // use an aria-label if you are omitting text on the button
+        // and using a sun/moon icon, for example
+        buttonEl.setAttribute("aria-label", newCta);
+        buttonEl.querySelector('.sr-only').innerText = newCta;
+    }
 
-  // update the button text
-  const newCta = newTheme === "dark" ? "Change to light theme" : "Change to dark theme";
-  button.innerText = newCta;  
+    /* fonction pour mettre à jour l'attribut sur l'élément html */
+    function updateThemeOnHtmlEl({ theme }) {
+        document.querySelector("html").setAttribute("data-theme", theme);
+    }
 
-  // use an aria-label if you are omitting text on the button
-  // and using sun/moon icons, for example
-  button.setAttribute("aria-label", newCta);
+    /* fonction principale, au chargement de la page */
+    function switchColor() {
+        /* récupération des éléments */
+        const buttons = document.getElementById("contraste");
+        const localStorageTheme = localStorage.getItem("theme");
+        const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
 
-  // update theme attribute on HTML to switch theme in CSS
-  document.querySelector("html").setAttribute("data-theme", newTheme);
+        /* récupération de l'état actuel du thème du site */
+        let currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme, systemSettingDark });
 
-  // update in local storage
-  localStorage.setItem("theme", newTheme);
+        if (buttons) {
+            buttons.forEach((button) => {
+                /* mise à jour du bouton en fonction du thème */
+                updateButton({ buttonEl: button, isDark: currentThemeSetting === "dark" });
+                updateThemeOnHtmlEl({ theme: currentThemeSetting });
 
-  // update the currentThemeSetting in memory
-  currentThemeSetting = newTheme;
+                /* event listener au click sur le bouton pour changer de thème */
+                button.addEventListener("click", (event) => {
+                    const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
+
+                    localStorage.setItem("theme", newTheme);
+                    updateButton({ buttonEl: button, isDark: newTheme === "dark" });
+                    updateThemeOnHtmlEl({ theme: newTheme });
+
+                    currentThemeSetting = newTheme;
+                });
+            });
+        }
+    }
+    switchColor();
 });
