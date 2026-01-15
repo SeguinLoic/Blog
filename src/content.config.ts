a3471f8d8293fec85d96c7c81002e144744f0a57
Loïc Seguin <seguin.loic@proton.me>
2026-01-15 11:32:03 +0100

    test

---
+++ src/content.config.ts
@@ -0,0 +1,14 @@
+import { defineCollection } from "astro:content";
+import { z } from "astro/zod";
+
+const blogCollection = defineCollection({
+  schema: ({ image }) => z.object({
+    title: z.string(),
+    image: image(),
+    imageAlt: z.string(),
+  }),
+});
+
+export const collections = {
+  blog: blogCollection,
+};
