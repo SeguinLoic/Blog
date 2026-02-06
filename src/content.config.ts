a399f002e5db4554de5e666b05b1de3eff8997dd
Loïc Seguin <seguin.loic@proton.me>
2026-02-06 11:14:37 +0100

    Edit

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,7 +3,7 @@ import { glob } from 'astro/loaders';
 import { z } from "astro/zod";
 
 const blogCollection = defineCollection({
-  loader: glob({ pattern: "**/*.mdx", base: "./src/content/blog"}),
+  loader: glob({ pattern: "**/*.mdx", base: "./src/content/blog" }),
   schema: ({ image }) => z.object({
     title: z.string(),
     pubDate: z.date(),
