0dd1d63aef97f81d2956479a75c2b2cff6fd7eb3
Loïc Seguin <seguin.loic@proton.me>
2026-02-06 09:16:51 +0100

    Tags

--- src/content.config.ts
+++ src/content.config.ts
@@ -6,6 +6,7 @@ const blogCollection = defineCollection({
     title: z.string(),
     pubDate: z.date(),
     description: z.string(),
+    tags: z.array(z.string()),
     cover: image()
   }),
 });
