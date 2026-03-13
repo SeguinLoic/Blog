f20cb383ab0c272ffb44bf6a830ba02fc6948ffe
Loïc Seguin <seguin.loic@proton.me>
2026-03-13 11:55:09 +0100

    Edit cursor

--- src/scripts/cursorMove.js
+++ src/scripts/cursorMove.js
@@ -1,16 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     const cursor = document.querySelector(".cursor");
+    const fenetre = document.querySelector(".fenetre");
 
     function updateDisplay(event) {
         let x = event.pageX;
         let y = event.pageY;
         cursor.style.top = y + 'px';
         cursor.style.left = x + 'px';
-        console.log(x);
-        console.log(y);
     }
 
-    document.addEventListener("mousemove", updateDisplay);
-    
+    fenetre.addEventListener("mousemove", updateDisplay);
+
 });
